fa0fe514d0276ded03efb00b3bc80a4c3b4ff47a
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-10-18 17:16:16 +0200

    Prepare for Flarum beta 14

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,24 +1,25 @@
+import app from 'flarum/app';
 import {extend} from 'flarum/extend';
 import PostUser from 'flarum/components/PostUser';
+import Link from 'flarum/components/Link';
 
 app.initializers.add('clarkwinkelmann-circle-groups', () => {
     extend(PostUser.prototype, 'view', function (vnode) {
-        const user = this.props.post.user();
+        const user = this.attrs.post.user();
 
         // If the post belongs to a deleted user, skip
         if (!user) {
             return;
         }
 
-        // Because badges aren't an actual model in the store,
-        // we read the ItemList containing the badge components
-        // That way we can also support third-party tags that might have been added
-        const firstColoredBadge = user.badges().toArray().find(badge => {
-            return badge.props.style && badge.props.style.backgroundColor;
+        // Find the first group that has a color
+        // We don't read badges because we would need to support every badge component and its attrs
+        const firstColoredGroup = user.groups().find(group => {
+            return group.color();
         });
 
-        // If there are no color badges, skip
-        if (!firstColoredBadge) {
+        // If there are no color groups, skip
+        if (!firstColoredGroup) {
             return;
         }
 
@@ -31,11 +32,11 @@ app.initializers.add('clarkwinkelmann-circle-groups', () => {
         };
 
         const avatar = vnode.children.find(matchTag('h3'))
-            .children.find(matchTag('a'))
+            .children.find(matchTag(Link))
             .children.find(matchClass('Avatar PostUser-avatar'));
 
         avatar.attrs = avatar.attrs || {};
         avatar.attrs.style = avatar.attrs.style || {};
-        avatar.attrs.style.borderColor = firstColoredBadge.props.style.backgroundColor;
+        avatar.attrs.style.borderColor = firstColoredGroup.color();
     });
 });
